6cfbac5511c4020c949ef8c146a6e77051f394b4
shoto88 <shotoharu@icloud.com>
2025-01-05 19:26:07 +0900

    first commit

--- schema_tickets2.sql
+++ schema_tickets2.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tickets (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   ticket_number INTEGER NOT NULL,
-  line_user_id TEXT NOT NULL,
+  line_user_id TEXT NOT NULL UNIQUE,
   line_display_name TEXT,
   ticket_time TEXT NOT NULL
 );
